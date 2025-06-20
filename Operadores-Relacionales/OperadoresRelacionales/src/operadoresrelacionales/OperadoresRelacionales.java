64a7feae0016d781d2ee9564e1460128655383c3
elpeluu <dgarporto@gmail.com>
2025-06-20 15:55:08 +0100

    Project Addition

---
+++ Operadores-Relacionales/OperadoresRelacionales/src/operadoresrelacionales/OperadoresRelacionales.java
@@ -0,0 +1,19 @@
+package operadoresrelacionales;
+
+public class OperadoresRelacionales {
+
+    public static void main(String[] args) {
+        int a = 10, b = 5;
+        
+        // Operadores relacionales
+        System.out.println(" a = " + a);
+        System.out.println(" b = " + b);
+        
+        System.out.println("a == b " + (a == b));
+        System.out.println("a < b " + (a < b));
+        System.out.println("a <= b " + (a <= b));
+        System.out.println("a > b " + (a > b));
+        System.out.println("a >= b " + (a >= b));
+        System.out.println("a != b " + (a !=b));
+    }  
+}
