046946b46f47acb3d66577c304baf1b987e89d53
elpeluu <dgarporto@gmail.com>
2025-06-21 18:23:55 +0100

    Vectores - Ejercicio 1: vector inverso

--- Vectores/src/main/java/com/mycompany/vectores/Vectores.java
+++ Vectores/src/main/java/com/mycompany/vectores/Vectores.java
@@ -3,6 +3,22 @@ package com.mycompany.vectores;
 public class Vectores {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Ejercicios de vectores
+        System.out.println("EJERCICIOS DE VECTORES Y MATRICES");
+        
+        //Ejercicio 1: Devolver un vector inversamente
+        vectorInverso();
+    }
+    
+    public static void vectorInverso(){
+        int v1[] = {1, 2, 3, 4, 5};
+        
+        System.out.println("***************************");
+        System.out.println("EJERCICIO 1: VECTOR INVERSO");
+        System.out.println("***************************");
+        
+        for(int i = v1.length-1; i >= 0; i--){
+            System.out.print(v1[i] + " ");
+        }
     }
 }
