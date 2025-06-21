2abf2cd23f83300ef765b710dc53b800502d6ca2
elpeluu <dgarporto@gmail.com>
2025-06-21 18:37:26 +0100

    Vectores - Ejercicio 2: Buscar valor

--- Vectores/src/main/java/com/mycompany/vectores/Vectores.java
+++ Vectores/src/main/java/com/mycompany/vectores/Vectores.java
@@ -8,8 +8,11 @@ public class Vectores {
         
         //Ejercicio 1: Devolver un vector inversamente
         vectorInverso();
+        //Ejercicio 2: Buscar en un vector
+        buscarValor();
     }
-    
+    ////////////////////////////////////////////////////////////////////////////
+    //Ejercicio 1: Devolver un vector inversamente
     public static void vectorInverso(){
         int v1[] = {1, 2, 3, 4, 5};
         
@@ -19,6 +22,33 @@ public class Vectores {
         
         for(int i = v1.length-1; i >= 0; i--){
             System.out.print(v1[i] + " ");
+        }
+        
+        System.out.println("\n\n");
+    }
+    
+    ////////////////////////////////////////////////////////////////////////////
+    //Ejercicio 2: Buscar en un vector
+    public static void buscarValor(){
+        int vector[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int valor = 69;
+        
+        System.out.println("*************************");
+        System.out.println("EJERCICIO 2: BUSCAR VALOR");
+        System.out.println("*************************");
+        
+        for(int i = 0; i < vector.length; i ++){
+                System.out.print(vector[i] + " ,");
+        }
+        System.out.println("");
+        
+        for(int i = 0; i < vector.length; i ++){
+            if(valor == vector[i]){
+                System.out.println(" El valor " + valor + " SI que se encuentra en la matriz");
+                break;
+            }else if(i == vector.length-1){
+                System.out.println(" El valor " + valor + " NO que se encuentra en la matriz");
+            }
         }
     }
 }
