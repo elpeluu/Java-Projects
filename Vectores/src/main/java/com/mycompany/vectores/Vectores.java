4f23dd3e5e0d29241d1048b1e5170b4235e2be24
elpeluu <dgarporto@gmail.com>
2025-07-08 11:15:09 +0200

    Vectores - Ejercicio 3: Buscar Pares

--- Vectores/src/main/java/com/mycompany/vectores/Vectores.java
+++ Vectores/src/main/java/com/mycompany/vectores/Vectores.java
@@ -10,6 +10,8 @@ public class Vectores {
         vectorInverso();
         //Ejercicio 2: Buscar en un vector
         buscarValor();
+        //Ejercicio 3: Buscar pares
+        buscarPares();
     }
     ////////////////////////////////////////////////////////////////////////////
     //Ejercicio 1: Devolver un vector inversamente
@@ -48,6 +50,37 @@ public class Vectores {
                 break;
             }else if(i == vector.length-1){
                 System.out.println(" El valor " + valor + " NO que se encuentra en la matriz");
+            }
+        }
+        System.out.println("\n\n");
+    }
+    
+    ////////////////////////////////////////////////////////////////////////////
+    //Ejercicio 3: Buscar valores pares dentro de un vector
+    public static void buscarPares(){
+        int[] v1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        
+        System.out.println("*************************");
+        System.out.println("EJERCICIO 3: BUSCAR PARES");
+        System.out.println("*************************");
+        
+        System.out.println("El vector en cuestion es: ");
+        for (int i = 0; i < v1.length; i++){
+            if(i == v1.length - 1){
+                System.out.print(v1[i] + " ");
+            }else{
+                System.out.print(v1[i] + ", ");
+            }
+        }
+        System.out.println("\n");
+        System.out.println("Busqueda de numeros pares:");        
+        for(int i = 0; i < v1.length; i++){
+            if(v1[i] % 2 == 0){
+                if(v1[i] == v1.length - 1){
+                    System.out.print(v1[i] + " ");
+                }else{
+                    System.out.print(v1[i] + ", ");
+                }
             }
         }
     }
