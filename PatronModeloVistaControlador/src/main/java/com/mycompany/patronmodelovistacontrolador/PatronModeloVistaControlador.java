52b4c4dfedf9e22c4e85c564486853b354d18af5
elpeluu <dgarporto@gmail.com>
2025-07-09 12:30:41 +0200

    Eliminacion de comentarios iniciales.

--- PatronModeloVistaControlador/src/main/java/com/mycompany/patronmodelovistacontrolador/PatronModeloVistaControlador.java
+++ PatronModeloVistaControlador/src/main/java/com/mycompany/patronmodelovistacontrolador/PatronModeloVistaControlador.java
@@ -1,16 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.patronmodelovistacontrolador;
 
-/**
- *
- * @author Usuario
- */
 public class PatronModeloVistaControlador {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
     }
 }
