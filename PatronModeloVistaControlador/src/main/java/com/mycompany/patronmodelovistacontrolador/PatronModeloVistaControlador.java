0f154bbe7ec13d300ff9e80524ddfafadd4d6bf6
elpeluu <dgarporto@gmail.com>
2025-07-09 12:30:09 +0200

    Agregación Proyecto PatronModeloVistaControlador

---
+++ PatronModeloVistaControlador/src/main/java/com/mycompany/patronmodelovistacontrolador/PatronModeloVistaControlador.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.patronmodelovistacontrolador;
+
+/**
+ *
+ * @author Usuario
+ */
+public class PatronModeloVistaControlador {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
