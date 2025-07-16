fcf22701dae8932aa122d932533c27622f6c7e99
elpeluu <dgarporto@gmail.com>
2025-07-16 11:48:22 +0200

    Ejercicios 1 y 2 - Saludar y despedirse con y sin parametros.

--- ClasesObjetos/src/clasesobjetos/ClasesObjetos.java
+++ ClasesObjetos/src/clasesobjetos/ClasesObjetos.java
@@ -12,7 +12,42 @@ public class ClasesObjetos {
         peluu.imprimirDatos();
         System.out.println("");
         lau.imprimirDatos();
+        System.out.println("");
+        
+        System.out.println("*****************************************");
+        System.out.println("EJERCICIO 1: Metodos saludar y despedirse");
+        System.out.println("*****************************************");
+        saludar();
+        despedirse();
+        System.out.println("");
+        
+        System.out.println("********************************************************");
+        System.out.println("EJERCICIO 2: Metodos saludar y despedirse con parametros");
+        System.out.println("********************************************************");
+        String nombreM = "Manu";
+        saludarParam(nombreM);
+        despedirseParam(nombreM);
+        System.out.println("");
     }  
     
+    // ******************************************
+    // EJERCICIO 1: Metodos saludar y despedirse
+    // ******************************************
+    public static void saludar(){
+        System.out.println("Pelu te saluda.");
+    }
+    public static void despedirse(){
+        System.out.println("Pelu se despide.");
+    }
+    
+    // ********************************************************
+    // EJERCICIO 2: Metodos saludar y despedirse con parametros
+    // ********************************************************
+    public static void saludarParam(String name){
+        System.out.println("Hola " + name + ", Peluu te saluda");
+    }
+    public static void despedirseParam(String name){
+        System.out.println("Adios " + name + ", Peluu te despide");
+    }
     
 }
